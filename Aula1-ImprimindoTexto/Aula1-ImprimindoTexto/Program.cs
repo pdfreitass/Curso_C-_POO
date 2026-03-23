816d974e6da4b7f02d8d9c1b43ef256c2a270ddf
Pedro Freitas <pedrofreitas.contato10@gmail.com>
2026-03-23 00:03:27 -0300

    Adixionando Aula 01 Imprimindo Textos

---
+++ Aula1-ImprimindoTexto/Aula1-ImprimindoTexto/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1_ImprimindoTexto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, Mundo!");
+            Console.WriteLine("Tudo bem com você !?");
+
+            Console.Write("Meu nome");
+            Console.Write(" É ");
+            Console.Write("\r\n");
+            Console.Write("Joao");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
