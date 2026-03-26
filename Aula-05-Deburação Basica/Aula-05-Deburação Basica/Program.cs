16ddddc2dab1a0e7ee2786ee48d73e9b04b3c14a
Pedro Freitas <pedrofreitas.contato10@gmail.com>
2026-03-25 22:33:30 -0300

    Aula sobre Depuração - O que é -Como depurar um código -Como adicionar break points - o que é watcher - janela de variaveis locais

---
+++ Aula-05-Deburação Basica/Aula-05-Deburação Basica/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_05_Deburação_Basica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu nome completo: ");
+            string NomeCompleto = Console.ReadLine();
+
+            Console.WriteLine("Digite sua idade: ");
+            int Idade = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digire o número do seu documento: ");
+            int NumeroDocumento = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o nome da sua rua: ");
+            string NomeRua = Console.ReadLine();
+
+            Console.WriteLine("Digite o número da sua casa: ");
+            int NumeroCasa = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Informe seu gênero: Pressione F para feminino e M para masculino ");
+            char genero = Convert.ToChar(Console.ReadKey(true).KeyChar);
+
+
+            Console.WriteLine("------------------------------");
+            Console.WriteLine($"Olá {NomeRua}");
+            Console.WriteLine($"Você tem {Idade} anos");
+            Console.WriteLine($"O número do seu documento é {NumeroDocumento}");
+            Console.WriteLine($"O nome da sua rua é {NomeRua}");
+            Console.WriteLine($"O número da sua casa é {NumeroCasa}");
+            Console.WriteLine($"O gênero informado é {genero}");
+            Console.WriteLine("------------------------------");
+
+            Console.WriteLine("Pressione qualquer tecla para encerrar o projeto: ");
+
+            Console.ReadKey();
+
+        }
+    }
+}
